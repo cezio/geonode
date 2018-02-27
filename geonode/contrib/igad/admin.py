9ce6d666b528d2b18c68affed61c1e6f49398704
Cezary Statkiewicz <cezio@thelirium.net>
2018-02-27 18:38:19 +0100

    igad app skeleton, hkeyword meta importer #33

---
+++ geonode/contrib/igad/admin.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+from django.contrib import admin
+
+from geonode.contrib.igad.models import HierarchicalKeywordMeta
+
+
+@admin.register(HierarchicalKeywordMeta)
+class HierarchicalKeywordMetaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'hkeyword',)
+    #fields = ('hkeyword', 'hkeyword__name', 'title', 'icon', 'description', 'url',)
+    #raw_id_fields = ('hkeyword',)
